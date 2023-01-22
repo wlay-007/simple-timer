b71bf6c4367de8bc7566499cb7af4a70f612625d
trinity - zakhar <54502101+wlay-007@users.noreply.github.com>
2023-01-22 03:42:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const timeBox = document.querySelector('.timer__seconds-count');
+const buttonStart = document.querySelector('#start-button');
+const buttonStop = document.querySelector('#stop-button');
+const buttonClear = document.querySelector('#clear-button');
+let buttonTest = true;
+let seconds = 0;
+let timerID;
+
+//start button
+buttonStart.addEventListener('click', function(){
+    if(buttonTest){
+        timerID = setInterval(function(){
+            seconds += 1;
+            timeBox.innerText = seconds;
+            buttonTest = false;
+        }, 100)
+    }
+})
+//stop button
+buttonStop.addEventListener('click', function(){
+    clearInterval(timerID);
+    buttonTest = true;
+})
+// clear button
+buttonClear.addEventListener('click', function(){
+    seconds = 0;
+    clearInterval(timerID);
+    timeBox.innerText = seconds;
+    buttonTest = true;
+})
